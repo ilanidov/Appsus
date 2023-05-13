a3885a7c60c8779f02dea600b9532a6be4f86006
Shira Cohen <shira140@gmail.com>
2023-05-13 12:24:47 +0300

    home , about pages

--- views/about.jsx
+++ views/about.jsx
@@ -1,6 +1,8 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <h1 className="about-header">Our mission is to organize the world’s information
+         and make it universally accessible and useful.</h1>
+        <section className="about-picture"></section>
     </section>
 }
--- views/home.jsx
+++ views/home.jsx
@@ -1,6 +1,7 @@
 
 export function Home() {
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+        <h1 className="main-slogen">AppSus</h1>
+        <button className="gmail-icon-home"></button>
     </section>
 }
